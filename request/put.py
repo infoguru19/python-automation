13a9719f7b305c8d5ab6fc6c4ce39086aeb60961
Ravi Ranjan <51418554+infoguru19@users.noreply.github.com>
2026-04-02 14:46:38 +0530

    Update put.py

--- request/put.py
+++ request/put.py
@@ -7,9 +7,7 @@ updated_post = {
     "body": "This content has been fully updated.",
     "userId": 1
 }
-
-response = requests.put(
-    "https://jsonplaceholder.typicode.com/posts/1",  # update post with id=1
-    json=updated_post,
-    verify=False
-)
+ 
+response = requests.put( "https://jsonplaceholder.typicode.com/posts/1", json=updated_post, verify=False) # update post with id=1
+print(response.status_code)   # 200 means success
+print(response.json())
